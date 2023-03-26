ac4dd4ae98a811bce3014d95baa99321486ae06a
Didi <121518338+devicplan@users.noreply.github.com>
2023-03-26 19:49:58 +0200

    Create secrets.py

---
+++ secrets.py
@@ -0,0 +1,8 @@
+# WOL with Raspberry Pico W 
+# Micropython with Raspberry Pico W
+# 26.03.2023 jd@icplan.de
+
+ssid = 'Your_WLAN_Name'
+password = 'Your_WLAN_Password'
+mac1 = [0x38,0xD2,0x01,0x35,0x1D,0x9A]                                              # PC 1
+mac2 = [0x38,0xD2,0x11,0x22,0x33,0x44]                                              # NAS 1
