f3cf3c9963ca5184e0c83a8cb903de948e4814fa
Didi <121518338+devicplan@users.noreply.github.com>
2023-03-26 19:55:26 +0200

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -4,5 +4,6 @@
 
 ssid = 'Your_WLAN_Name'
 password = 'Your_WLAN_Password'
+broadcast = "192.168.0.255"                                                         # broadcast ip
 mac1 = [0x38,0xD2,0x01,0x35,0x1D,0x9A]                                              # PC 1
 mac2 = [0x38,0xD2,0x11,0x22,0x33,0x44]                                              # NAS 1
